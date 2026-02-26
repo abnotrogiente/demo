bc22b85f18c138a7b1b6cf90198f6a6f7e8f49c8
abnotrogiente <abnotrogiente@gmail.com>
2026-02-26 16:23:38 +0100

    adjusted shadow circle radius

--- swimming/params.js
+++ swimming/params.js
@@ -2,7 +2,7 @@ let params = {
     numSteps: 2, focal: 45,
     simulation: { optimized: false },
     sparks: { enabled: false, glow: 5., glowOffset: .5, lengthFactor: 1., stroke: .01, num: 40, sizeFactor: 50, fov: Math.PI / 4 },
-    shadow: { enabled: true, shadowRadius: .5, shadowPower: .5, showCircle: true, circleRadius: 1., circleStroke: .15 }
+    shadow: { enabled: true, shadowRadius: .5, shadowPower: .5, showCircle: true, circleRadius: .6, circleStroke: .15 }
 };
 
 export { params };
